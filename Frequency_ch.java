d81a4807224bb2bee67ccb137fbaa6770354237f
Rushikesh Mind <157093618+RusheeMind55@users.noreply.github.com>
2024-04-14 20:57:44 +0530

    Add files via upload

---
+++ Frequency_ch.java
@@ -0,0 +1,20 @@
+package Homework_Array;
+
+public class Frequency_ch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		for(int i=1;i<=5;++i) {
+			
+			for(int j=1;j<=5;++j)
+			{
+				System.out.print(j);
+
+			}
+		
+		System.out.println();
+
+}}}
